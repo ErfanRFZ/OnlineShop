4ac8a5f1da1a7fe90b0d0a260e85877ed70ddcab
Erfan Rafezi <53901928+ErfanRFZ@users.noreply.github.com>
2021-10-21 21:19:35 +0300

    Setup Project and did the loading and login form.

---
+++ OnlineShop/Loading.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace OnlineShop
+{
+    partial class Loading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.versioLabel = new System.Windows.Forms.Label();
+            this.onlineshopLabel = new System.Windows.Forms.Label();
+            this.progressBar = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::OnlineShop.Properties.Resources.Shop150;
+            this.pictureBox1.Location = new System.Drawing.Point(63, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 181);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // versioLabel
+            // 
+            this.versioLabel.AutoSize = true;
+            this.versioLabel.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.versioLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(114)))), ((int)(((byte)(255)))));
+            this.versioLabel.Location = new System.Drawing.Point(272, 150);
+            this.versioLabel.Name = "versioLabel";
+            this.versioLabel.Size = new System.Drawing.Size(106, 28);
+            this.versioLabel.TabIndex = 13;
+            this.versioLabel.Text = "version 1.0";
+            this.versioLabel.Click += new System.EventHandler(this.versioLabel_Click);
+            // 
+            // onlineshopLabel
+            // 
+            this.onlineshopLabel.AutoSize = true;
+            this.onlineshopLabel.Font = new System.Drawing.Font("Segoe UI", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onlineshopLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(0)))), ((int)(((byte)(153)))));
+            this.onlineshopLabel.Location = new System.Drawing.Point(254, 88);
+            this.onlineshopLabel.Name = "onlineshopLabel";
+            this.onlineshopLabel.Size = new System.Drawing.Size(295, 62);
+            this.onlineshopLabel.TabIndex = 12;
+            this.onlineshopLabel.Text = "Online Shop";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(0, 439);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(0)))), ((int)(((byte)(153)))));
+            this.progressBar.ProgressColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(114)))), ((int)(((byte)(255)))));
+            this.progressBar.ShadowDecoration.Parent = this.progressBar;
+            this.progressBar.Size = new System.Drawing.Size(800, 13);
+            this.progressBar.TabIndex = 15;
+            this.progressBar.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Loading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.versioLabel);
+            this.Controls.Add(this.onlineshopLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Loading";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loading";
+            this.Load += new System.EventHandler(this.Loading_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label versioLabel;
+        private System.Windows.Forms.Label onlineshopLabel;
+        private Guna.UI2.WinForms.Guna2ProgressBar progressBar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ OnlineShop/Loading.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlineShop
+{
+    public partial class Loading : Form
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+
+        private void versioLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int startPoint = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startPoint += 1;
+            progressBar.Value = startPoint;
+            if (progressBar.Value == 100)
+            {
+                progressBar.Value = 0;
+                timer1.Stop();
+                Form1 log = new Form1();
+                this.Hide();
+                log.Show();
+            }
+        }
+
+        private void Loading_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
